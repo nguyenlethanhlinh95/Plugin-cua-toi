030498a85734f270c5d90f33842c9f6c332d33f4
Thanh Linh <nguyenlethanhlinh95@gmail.com>
2019-06-12 21:07:36 +0700

    first commit

---
+++ learn-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: My First learn plugin
+ * Plugin URI: http://skytechkey.com // Địa chỉ trang chủ của plugin
+ * Description: Đây là plugin đầu tiên mà tôi viết dành riêng cho WordPress, chỉ để học tập mà thôi. // Phần mô tả cho plugin
+ * Version: 1.0 // Đây là phiên bản đầu tiên của plugin
+ * Author: Thanh Linh // Tên tác giả, người thực hiện plugin này
+ * Author URI: http://skytechkey.com // Địa chỉ trang chủ của tác giả
+ * License: GPLv2 or later // Thông tin license của plugin, nếu không quan tâm thì bạn cứ để GPLv2 vào đây
+ */
